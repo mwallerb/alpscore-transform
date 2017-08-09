fa776f003d6c6a652f64c5f8a7edcc3c9170f155
Markus Wallerberger <mwallerb@umich.edu>
2017-08-09 12:05:50 +0900

    Init structure part 2.

---
+++ include/alps/transform/fourier.hpp
@@ -0,0 +1,12 @@
+/*
+ * Copyright (C) 1998-2017 ALPS Collaboration. See COPYRIGHT.TXT
+ * All rights reserved. Use is subject to license terms. See LICENSE.TXT
+ * For use in publications, see ACKNOWLEDGE.TXT
+ */
+#ifndef ALPS_TRANSFORM_FOURIER_HPP
+#define ALPS_TRANSFORM_FOURIER_HPP
+
+int tere();
+
+#endif
+
